db75bf8a860cb63971d361aecd7833d2ead04079
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-04-10 00:23:33 -0300

    Finish all tests

--- src/app.js
+++ src/app.js
@@ -1,33 +1,105 @@
-const express = require("express");
-const cors = require("cors");
+const express = require( "express" );
+const cors = require( "cors" );
 
-// const { uuid } = require("uuidv4");
+const {
+  uuid,
+  isUuid
+} = require( "uuidv4" );
 
 const app = express();
 
-app.use(express.json());
-app.use(cors());
+app.use( express.json() );
+app.use( cors() );
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
-});
+const validateProjectId = ( request, response, next ) => {
+  const {
+    id
+  } = request.params
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
+  if ( !isUuid( id ) ) {
+    return response.status( 400 ).json( {
+      error: 'Invalid repository ID'
+    } )
+  }
+  return next()
+}
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.get( "/repositories", ( request, response ) => {
+  return response.json( repositories )
+} );
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.post( "/repositories", ( request, response ) => {
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
-});
+  const {
+    title,
+    url,
+    techs,
+  } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  }
+
+  repositories.push( repository )
+
+  return response.json( repository )
+
+} );
+
+app.put( "/repositories/:id", validateProjectId, ( request, response ) => {
+  const {
+    id
+  } = request.params
+
+  const {
+    title,
+    url,
+    techs
+  } = request.body
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id )
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repositoryIndex].likes
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.json( repository )
+
+} );
+
+app.delete( "/repositories/:id", validateProjectId, ( request, response ) => {
+  const {
+    id
+  } = request.params
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id )
+
+  repositories.splice( repositoryIndex, 1 )
+
+  return response.status( 204 ).send()
+} );
+
+app.post( "/repositories/:id/like", validateProjectId, ( request, response ) => {
+  const {
+    id
+  } = request.params
+
+  const repository = repositories.find( repository => repository.id === id )
+
+  repository.likes += 1
+  return response.send( repository )
+} );
 
 module.exports = app;
